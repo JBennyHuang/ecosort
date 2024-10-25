12f027d67e6ca5e22271eb3d1e1cfdd8868c675c
jbennyhuang <j.benny.huang.developer@gmail.com>
2024-10-25 10:39:48 -0400

    added points increment path

--- .backend/src/functions/users.ts
+++ .backend/src/functions/users.ts
@@ -1,7 +1,14 @@
 import UserController from "../controllers/user";
 import { app } from "@azure/functions";
 
+// /users
+
 app.get("users-get-me", {
   route: "users/me",
   handler: UserController.getMe,
+});
+
+app.patch("increment-user-points", {
+  route: "users/increment",
+  handler: UserController.incrementByOne,
 });
